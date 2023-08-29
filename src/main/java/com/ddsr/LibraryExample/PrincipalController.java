807f870bf7888b8c0519ce6d60fbc20e5b24d1e3
DDSR <damian@d2600.com>
2023-08-29 00:10:55 -0700

    adding a functional postmapping method on rest controller function

--- src/main/java/com/ddsr/LibraryExample/PrincipalController.java
+++ src/main/java/com/ddsr/LibraryExample/PrincipalController.java
@@ -1,5 +1,35 @@
 package com.ddsr.LibraryExample;
 
 
+import com.ddsr.LibraryExample.entity.Books;
+import com.ddsr.LibraryExample.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//import java.awt.print.Book;
+
+@RestController
 public class PrincipalController {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public PrincipalController (BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping (value = "/saveBook")
+    public String addBook (@RequestBody Books books){
+        bookRepository.save(books);
+        return "Libro salvados";
+    }
+    @PostMapping (value = "/printBook")
+    public List<Books> addBook (){
+        return bookRepository.findAll();
+//        return "Libro salvados";
+    }
 }
--- src/main/java/com/ddsr/LibraryExample/repository/BookRepository.java
+++ src/main/java/com/ddsr/LibraryExample/repository/BookRepository.java
@@ -4,5 +4,5 @@ import com.ddsr.LibraryExample.entity.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Books, Integer> {
+public interface BookRepository extends JpaRepository<Books, Integer> {
 }
